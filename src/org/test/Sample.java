ed1e3cba9bc7d76f5f6f8f40f2cdc20b63c7c41c
unknown <vinithravindran06@gmail.com>
2021-11-12 19:27:27 +0530

    Login Details

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Sample {
-
+	
+	public void login() {
+		System.out.println("Login Details");
+	}
+	
+	public static void main(String[] args) {
+		Sample z = new Sample();
+		z.login();
+	}
 }
