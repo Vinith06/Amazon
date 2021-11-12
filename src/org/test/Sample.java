16f552042cd4bedc95a0f4513ae80d63457b51ad
unknown <vinithravindran06@gmail.com>
2021-11-12 19:40:16 +0530

    UserName Details

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Sample {
-
+	
+	public void userName() {
+		System.out.println("UserName Details");
+	}
+	
+	public static void main(String[] args) {
+		Sample a = new Sample();
+		a.userName();
+	}
 }
